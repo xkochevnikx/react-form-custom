93e7a2020bb87627f6eab5f9652797a05c600820
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-09-13 16:35:13 +0300

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: ['.eslintrc.{js,jsx}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['react'],
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
