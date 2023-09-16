d58f57df2bb937b641efc1fd3ea83424e218c547
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-09-16 12:43:43 +0300

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,6 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
         'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
     },
 };
